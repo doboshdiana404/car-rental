33808cde4649b34460e375176649f730a31595dc
diana_dobosh <diana.lakatosh.girl@gmail.com>
2025-05-18 02:51:50 +0300

    add animation to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,20 @@
 import { useNavigate } from 'react-router-dom';
 import s from './Hero.module.css';
+import { useState } from 'react';
 const Hero = () => {
   const navigate = useNavigate();
+  const [slideUp, setSlideUp] = useState(false);
 
   const handleClick = () => {
-    navigate('/catalog');
+    setSlideUp(true);
+
+    setTimeout(() => {
+      navigate('/catalog');
+    }, 600);
   };
+
   return (
-    <section className={s.hero}>
+    <section className={`${s.hero} ${slideUp ? s.slideUp : ''}`}>
       <div>
         <h1 className={s.heroTitle}>Find your perfect rental car</h1>
         <p className={s.heroDescription}>Reliable and budget-friendly rentals for any journey</p>
